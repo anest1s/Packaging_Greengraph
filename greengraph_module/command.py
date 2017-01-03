f0c700a67f2aee23ad8d03418c28340cf122b9d4
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-01-03 06:34:39 +0000

    Command line entry point created

--- greengraph_module/command.py
+++ greengraph_module/command.py
@@ -1,5 +1,22 @@
 
-from matplotlib import pyplot as plt
-mygraph = Greengraph('New York','Chicago')
-data = mygraph.green_between(21)
-plt.plot(data)
+from argparse import ArgumentParser
+import graph
+import matplotlib.pyplot as plt
+
+
+def process():
+   parser = ArgumentParser(description = "Generate a graph that shows \
+                    the amount of green pixels between two locations. ")
+   parser.add_argument('--fr0m', '-f', required=True)
+   parser.add_argument('--to', '-t', required=True)
+   parser.add_argument('--steps', '-s', required=True)
+   parser.add_argument('--out', '-o', required=True)
+   arguments = parser.parse_args()
+
+   mygraph = graph.Greengraph(arguments.fr0m, arguments.to)
+   data = mygraph.green_between(arguments.steps)
+   plt.plot(data)
+   plt.savefig(arguments.out)
+
+if __name__ == "__main__":
+    process()
