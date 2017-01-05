f4affb3057a209df8b37d50554e41dafa1894bbe
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-01-05 03:27:57 +0000

    Fixtures: location sequence

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -8,11 +8,18 @@ graph = Greengraph('Athens', 'Lamia')
 places = ['Athens', 'Lamia']
 steps = [2, 4]
 
-
-#geolocate fixture
+# geolocate fixtures
 fixtures = {}
 with open(os.path.join(os.path.dirname(__file__), 'fixtures.yaml'), 'w') as fixtures_file:
     for place in places:
         latitude, longitude = graph.geolocate(place)
         fixtures[place] = {'latitude': latitude, 'longitude': longitude}
     yaml.dump({'geolocate': fixtures}, fixtures_file)
+
+
+# location_sequence fixtures
+fixtures = {}
+with open(os.path.join(os.path.dirname(__file__), 'fixtures.yaml'), 'a') as fixtures_file:
+    for steps in steps:
+        fixtures[steps] = (graph.location_sequence(graph.geolocate('Athens'), graph.geolocate('Lamia'), steps)).tolist()
+    yaml.dump({'location_sequence': fixtures}, fixtures_file)
